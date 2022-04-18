505866a168247171f918b5c41afd316d0a0dab6d
Maria Zasypkina <maria.zasypkina@gmail.com>
2022-04-18 14:11:06 -0600

    april 18 2-09

--- Test_MZ.java
+++ Test_MZ.java
@@ -1,5 +1,7 @@
 public class Test_MZ {
     public static void main(String[] args) {
         System.out.println("Test test test");
+        System.out.println("Где я?)))");
+        System.out.println("mmm");
     }
 }
