3fc413ed74eda6aef08089d639c6f692b7c8c359
Anna Fedorova <snysmymric@gmail.com>
2022-04-19 00:46:33 -0700

    Delete First Java Project from repository

--- Test_MZ.java
+++
@@ -1,7 +0,0 @@
-public class Test_MZ {
-    public static void main(String[] args) {
-        System.out.println("Test test test");
-        System.out.println("Где я?)))");
-        System.out.println("Надеюсь, не накосячу");
-    }
-}
