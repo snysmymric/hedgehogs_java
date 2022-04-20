2e71999a82c361e897918e2415f82ff7d8110ab1
Anna Fedorova <snysmymric@gmail.com>
2022-04-19 21:58:01 -0700

    Added dependency TestNG, added method abbr and  testAbbr

--- hedgehogsMaven/src/main/java/MainApp.java
+++ hedgehogsMaven/src/main/java/MainApp.java
@@ -1,10 +1,15 @@
 public class MainApp {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println(Utils.getDate());
 
-
+        System.out.println(abbr("Sergey",4));
 
     }
+    public static String abbr (String str, int size) {
+       return str.substring(0,size - 3) + "...";
+
+    }
+
 
 }
--- hedgehogsMaven/src/main/java/NewAnna19_04.java
+++
@@ -1,5 +0,0 @@
-public class NewAnna19_04 {
-    public static void printName (String name) {
-        System.out.println(name);
-    }
-}
