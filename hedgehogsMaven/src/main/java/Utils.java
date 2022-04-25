0d7362bc27f8cc7c3689826d9ce0010c8820770f
Anna Fedorova <snysmymric@gmail.com>
2022-04-24 22:03:55 -0700

    Added methods getMaxValue, getMinValue, getMidValue, getMaxMinMedFromArray

--- hedgehogsMaven/src/main/java/Utils.java
+++ hedgehogsMaven/src/main/java/Utils.java
@@ -34,4 +34,67 @@ public class Utils {
     public static String compareToArray (int[] arr1, int[] arr2) {
         return Arrays.stream(arr1).sum() > Arrays.stream(arr2).sum() ? Arrays.toString(arr1) : Arrays.toString(arr2);
     }
+
+    public static int[] createRandomArray (int arraySize, int maxInt) {
+        int[] arrayR = new int[arraySize]; // add check on -2 maxInt must be
+        // positive
+        for (int i = 0; i < arrayR.length; i++) {
+            arrayR[i] = getRandom(maxInt);
+        }
+        return arrayR;
+    }
+
+    public static int getMidValue (int[] array) {
+        int mid = 0;
+        if (array.length == 0) {
+            return -1; //error
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                mid += array[i];
+            }
+        }
+        return mid/array.length;
+    }
+
+    public static int getMaxValue (int[] array) {
+        int max = Integer.MIN_VALUE;
+        if (array.length < 0) {
+            return -1;  //error
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int getMinValue (int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static int[] getMaxMinMedFromArray (int[] array) {
+        final int ARRAYSIZE = 3;
+        int[] arrayMaxMinMed = new int[ARRAYSIZE];
+            for (int i = 0; i < arrayMaxMinMed.length; i++) {
+                if (i == 0) {
+                    arrayMaxMinMed[i] = getMaxValue(array);
+                } else if (i == 1) {
+                    arrayMaxMinMed[i] = getMinValue(array);
+                } else {
+                    arrayMaxMinMed[i] = getMidValue(array);
+                }
+            }
+        return arrayMaxMinMed;
+        }
+
+
+
 }
--- hedgehogsMaven/src/test/java/MainTest.java
+++ hedgehogsMaven/src/test/java/MainTest.java
@@ -3,11 +3,6 @@ import org.testng.annotations.Test;
 
 public class MainTest {
 
-    @Test
-    public  void testAbbr() {
-        String result = MainApp.abbr("Sergey",4);
-        Assert.assertEquals(result, "S...");
-    }
 
 
 }
